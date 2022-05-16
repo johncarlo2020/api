c06506d0b2d85d0df429030153bcfa19093385ac
carlo <johncarlocasipit@gmail.com>
2022-05-16 17:03:30 +0800

    changes

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,5 +1,6 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-rose-900">
     <div>
+    
         {{ $logo }}
     </div>
 
