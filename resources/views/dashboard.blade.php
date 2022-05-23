d0fba1f4d89e0910cf463342be48a650da6130d9
carlo <johncarlocasipit@gmail.com>
2022-05-23 18:40:20 +0900

    status

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -32,7 +32,7 @@
                                     created at
                                 </th>
                                 <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                    updated at
+                                    Status
                                 </th>
                                 </tr>
                             </thead>
@@ -56,7 +56,7 @@
                                     {{$user->created_at}}
                                     </td>
                                     <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                    {{$user->updated_at}}
+                                    {{$user->status}}
                                     </td>
                                 </tr>
                                 @endforeach
