dbc4dde0b84dd92c5e217eb12e4ded5aa43b4530
carlo <johncarlocasipit@gmail.com>
2022-05-31 12:13:16 +0900

    datatable

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
                     <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
                         <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
                         <div class="overflow-hidden">
-                            <table class="min-w-full">
+                            <table id="dataTable" class="min-w-full">
                             <thead class="bg-white border-b">
                                 <tr>
                                 <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
@@ -88,4 +88,9 @@
     </div>
 </x-app-layout>
 
-</script>
+<script>
+            $(document).ready(function () {
+                $('#dataTable').DataTable();
+
+            });
+        </script>
