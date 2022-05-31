694d29fe686484fb0cf60c239d16c714ca91cc5e
carlo <johncarlocasipit@gmail.com>
2022-05-31 11:24:23 +0900

    jquery

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -86,7 +86,7 @@
         </div>
     </div>
 </x-app-layout>
-
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 <script>
     function preview(){
     var id  = $('.details').attr("id"); 
