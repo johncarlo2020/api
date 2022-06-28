091f9819c296c8ca88c428e24fe0d027b752c6d5
carlo <johncarlocasipit@gmail.com>
2022-06-28 09:35:43 +0800

    hi

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,8 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'subcription'=>'0',
+            'status'=>'1',
             'password' => Hash::make($data['password']),
         ]);
     }
